0e435249d8f357f7691d851d793b0f8347962a61
AgusZanini <zaniniagustin06@gmail.com>
2020-05-05 20:11:24 -0300

    ejercicio grados a radianes agregado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -36,3 +36,16 @@ function conversorunidades(id, valor){
     }
 
 }
+
+function convertirGR(id){
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById( "grados").value;
+        rad = (grad*math.PI)/180;
+    }else if (id=="radianes"){
+        rad = document.getElementById( "radianes").value;
+        grad = (rad*180)/math.PI
+    }
+    document.getElementById( "grados").value = grad;
+    document.getElementById( "radianes").value = rad;
+}
